afe14fa8f2465d7ece393e7dd83cea50a5f8417f
lsroldan43 <LSROLDAN@davivienda.loc>
2021-11-29 11:34:57 -0500

    docs: Change the documentation on bikeshare.py
    docs: Change the documentation on bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -120,7 +120,7 @@ def time_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-
+# Function stats
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
