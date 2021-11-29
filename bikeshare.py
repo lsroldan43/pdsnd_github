f41e2df20d04c9a6fc92bd1359793afff2d4647a
Lizeth Roldan <LSROLDAN@davivienda.loc>
2021-11-29 09:36:05 -0500

    Fist commit for refactoring-
    Documentation of third fucntion of bikeshare.py. This function is for loading data

--- bikeshare.py
+++ bikeshare.py
@@ -62,7 +62,7 @@ def get_filters():
     return city, month, day
     #city, month, day=get_filters()
 
-
+# Function to load data
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
