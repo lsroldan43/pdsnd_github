6f610b3bdec02bbbd1d8a1b0410ef107f0fefb14
Lizeth Roldan <LSROLDAN@davivienda.loc>
2021-11-29 09:31:14 -0500

    bikehare.py:Fist comment on bikeshere.py Describing the first code lines, this lines extract the name of csv files to work with the names of the cities.

--- bikeshare.py
+++ bikeshare.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 import sys 
-
+# Extract cities names form file's csv names
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
